54422944a2e475407ef36e03cfb05b4282ac588f
BAS-team <BSA-team@outlook.com>
2021-03-29 11:01:53 +0800

    Add project

---
+++ BsaUsdtLpBsaPool_impl.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "https://github.com/BSA-team/BSAcontract/blob/main/BsaUsdtLpBsaPool_v_1.0.sol";
+
+contract BsaUsdtLpBsaPool_impl is BsaUsdtLpBsaPool {
+    /**
+     * constructor
+     *
+     * Requirements:
+     *
+     * - `bsa_` token address
+     * - `lptoken_` lptoken address
+     * - `starttime_` starttime
+     */
+    constructor(address bsa_, address lptoken_, uint256 starttime_) BsaUsdtLpBsaPool(bsa_,lptoken_,starttime_) public {
+
+    }
+}
